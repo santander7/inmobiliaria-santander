5e0e65d018442a15f610d197816e94a0ab018b31
Sebastian <jhonsantander2011@gmail.com>
2026-07-18 02:02:27 -0500

    Conectar frontend con el backend en Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Backend URL
+  baseURL: import.meta.env.PROD ? 'https://inmobiliaria-santander.onrender.com/api' : 'http://localhost:3000/api', // Backend URL
   headers: {
     'Content-Type': 'application/json'
   }
